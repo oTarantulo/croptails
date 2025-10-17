e5c5910635a4f2bbb82bcb6f78929b2cf01cf8f2
Tarântulo <236284661+oTarantulo@users.noreply.github.com>
2025-10-17 10:53:39 -0300

    Added tree shake shader. Fixed tutorial's timer not resetting.

--- scenes/characters/player/idle_state.gd
+++ scenes/characters/player/idle_state.gd
@@ -7,16 +7,7 @@ func _on_process(_delta : float) -> void:
 	pass
 
 func _on_physics_process(_delta : float) -> void:
-	if player.player_direction == Vector2.UP:
-		animated_sprite_2d.play("idle_back")
-	elif player.player_direction == Vector2.DOWN:
-		animated_sprite_2d.play("idle_front")
-	elif player.player_direction == Vector2.LEFT:
-		animated_sprite_2d.play("idle_left")
-	elif player.player_direction == Vector2.RIGHT:
-		animated_sprite_2d.play("idle_right")
-	else:
-		animated_sprite_2d.play("idle_front")
+	pass
 	
 func _on_next_transitions() -> void:
 	GameInputEvents.movement_input()
--- scenes/objects/trees/large_tree.gd
+++ scenes/objects/trees/large_tree.gd
@@ -2,15 +2,27 @@ extends Sprite2D
 
 @onready var hurt_component: HurtComponent = $HurtComponent
 @onready var damage_component: DamageComponent = $DamageComponent
+@onready var shake_timer: Timer = $ShakeTimer
 
 var log_scene = preload("res://scenes/objects/trees/log.tscn")
 
 func _ready() -> void:
 	hurt_component.hurt.connect(on_hurt)
 	damage_component.max_damage_reached.connect(on_max_damage_reached)
+	shake_timer.timeout.connect(_on_timer_timeout)
 
 func on_hurt(hit_damage: int) -> void:
+	shake_timer.stop()
 	damage_component.apply_damage(hit_damage)
+	material.reset_state()
+	material.set_shader_parameter("shake_intensity", 1.0)
+	#await get_tree().create_timer(1.0).timeout
+	#material.set_shader_parameter("shake_intensity", 0.5)
+	shake_timer.start()
+	
+func _on_timer_timeout() -> void:
+	material.set_shader_parameter("shake_intensity", 0.5)
+	print("Shake stopped!")
 
 func on_max_damage_reached() -> void:
 	print("Max damage reached!")
